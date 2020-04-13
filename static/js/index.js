ec4753bb24b3dfd52aa342b80b1d1513599fd966
zhouhuajian <zhouhuajian@website>
2020-04-13 15:03:51 +0800

    '开发头部动画'

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,18 @@
+var webNames = [
+    '未来不是梦',
+    '有梦才会赢'
+];
 $(function() {
+    var index = Math.floor(Math.random() * webNames.length);
+    $('#web-name').html(webNames[index]);
     setTimeout(function () {
-        $('#web-name').show().addClass('animated bounceInLeft');
+        $('#web-name-cover').animate({
+            left: '170px'
+        }, 800);
     }, 500);
     setTimeout(function () {
         $('#nav').show().addClass('animated bounceInRight');
-    }, 800)
+    }, 1300)
     setTimeout(function () {
         $('.share-wrap').show().addClass('animated fadeInUp');
         $('.foot-wrap').show().addClass('animated fadeInUp');
@@ -13,8 +21,8 @@ $(function() {
    $('.nav-btn').click(function () {
         var page = $(this).attr('page');
         $('.main-wrap').hide();
-        $('.foot-wrap').hide();
+        // $('.foot-wrap').hide();
         $('.' + page + '-wrap').show().addClass('animated fadeInUp');
-        $('.foot-wrap').show().addClass('animated fadeInUp');
+        // $('.foot-wrap').show().addClass('animated fadeInUp');
    });
 });
