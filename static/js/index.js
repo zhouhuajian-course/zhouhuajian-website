11e9b4517708f29f4d401882acd8b87ceadf5cfb
zhouhuajian <zhouhuajian@website>
2020-04-15 10:01:20 +0800

    '优化动画主题颜色'

--- static/js/index.js
+++ static/js/index.js
@@ -16,23 +16,35 @@ $(function() {
     setTimeout(function () {
         $('#web-name-cover').animate({
             left: '170px'
-        }, 800);
+        }, 1500);
     }, 500);
     setTimeout(function () {
         $('#nav').show().addClass('animated bounce-in-right');
     }, 1300);
     setTimeout(function () {
-        $('.share-wrap').show().addClass('animated fade-in-up fast');
-        $('.foot-wrap').show().addClass('animated fade-in-up fast');
+        // $('.share-wrap').show().addClass('animated fade-in-up fast');
+        $('.share-wrap').fadeIn();
+        // $('.foot-wrap').show().addClass('animated fade-in-up fast');
+        $('.foot-wrap').fadeIn();
     }, 1500);
 
+    // 导航按钮 鼠标click 鼠标hover
     $('.nav-btn').click(function () {
+        $(this).removeClass('animated bounce slow infinite');
         var page = $(this).attr('page');
         $('.main-wrap').hide();
         // $('.foot-wrap').hide();
-        $('.' + page + '-wrap').show().addClass('animated fade-in-up fast');
+        // $('.' + page + '-wrap').show().addClass('animated fade-in-up fast');
+        $('.' + page + '-wrap').fadeIn();
         // $('.foot-wrap').show().addClass('animated fadeInUp');
     });
+    $('.nav-btn').hover(function () {
+        $(this).siblings().removeClass('animated bounce slow infinite');
+        $(this).addClass('animated bounce slow infinite active');
+    }, function () {
+        $(this).removeClass('animated bounce slow infinite');
+    });
+
     $('.head .link').click(function () {
         location.reload();
     });
