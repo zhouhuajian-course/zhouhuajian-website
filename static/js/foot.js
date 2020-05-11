3ef81a1f032bd905b9737ba349e8fc0bd2c861ea
zhouhuajian <zhouhuajian@website>
2020-05-11 12:18:04 +0800

    '开发通用尾部页面'

---
+++ static/js/foot.js
@@ -0,0 +1,16 @@
+/**
+ * 通用尾部JS
+ *
+ * @link    http://zhouhuajian.website
+ * @package zhouhuajian.website
+ * @author  zhouhuajian
+ * @version v1.0
+ */
+// 百度统计JS
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?39ce32fb83f9524ce90a9aad7643d6b8";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
